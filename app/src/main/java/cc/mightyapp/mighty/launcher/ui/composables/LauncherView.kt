7033e9e2e3c3f1decfa2cb1669627646bd64db7b
Matt Ramotar <matt.ramotar@jhu.edu>
2021-05-29 17:12:35 -0400

    Style splash screen

--- app/src/main/java/cc/mightyapp/mighty/launcher/ui/composables/LauncherView.kt
+++ app/src/main/java/cc/mightyapp/mighty/launcher/ui/composables/LauncherView.kt
@@ -1,20 +1,26 @@
 package cc.mightyapp.mighty.launcher.ui.composables
 
+import android.view.Gravity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import cc.mightyapp.mighty.R
 import cc.mightyapp.mighty.launcher.data.entities.LaunchDestination
 import cc.mightyapp.mighty.launcher.ui.presenter.LauncherViewModel
+import dagger.Provides
 import kotlinx.coroutines.delay
 
 private const val SplashWaitTime: Long = 1000
@@ -54,15 +60,17 @@ fun LauncherView(
 
 @Composable
 fun ShowSplash(modifier: Modifier) {
-    Box(
+    Column(
         modifier = modifier
             .fillMaxSize()
-            .background(MaterialTheme.colors.background)
+            .background(MaterialTheme.colors.background),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
         Image(
             painter = painterResource(id = R.drawable.ic_mighty),
-            contentDescription = null
+            contentDescription = null,
         )
     }
 }
