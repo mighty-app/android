7eae685b9a60ae0f4ea1e0c8133d0d7f159c962e
Matt Ramotar <matt.ramotar@jhu.edu>
2021-05-30 21:47:40 -0400

    Add MightyChip component

---
+++ app/src/main/java/cc/mightyapp/mighty/common/ui/composables/components/MightyChip.kt
@@ -0,0 +1,45 @@
+package cc.mightyapp.mighty.common.ui.composables.components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun MightyChip(
+    isSelected: Boolean = false,
+    label: String = "Mighty Chip",
+    colorWhenSelected: Color = MaterialTheme.colors.secondary,
+    colorWhenNotSelected: Color = MaterialTheme.colors.secondaryVariant,
+) {
+    Surface(
+        modifier = Modifier.padding(8.dp),
+        elevation = 8.dp,
+        shape = RoundedCornerShape(16.dp),
+        border = BorderStroke(
+            width = 1.dp,
+            color = when {
+                isSelected -> colorWhenSelected
+                else -> colorWhenNotSelected
+            }
+        )
+    ) {
+        Row {
+            Text(
+                text = label,
+                style = MaterialTheme.typography.body2,
+                color = Color.White,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
