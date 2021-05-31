31e743f922a8c7530f61316b561024f66168f744
Matt Ramotar <matt.ramotar@jhu.edu>
2021-05-31 12:57:29 -0400

    Add fakes

---
+++ app/src/main/java/cc/mightyapp/mighty/common/data/fakes/FakeLevels.kt
@@ -0,0 +1,14 @@
+package cc.mightyapp.mighty.common.data.fakes
+
+import cc.mightyapp.mighty.common.data.models.Level
+
+object FakeLevels {
+
+    val LevelOne = Level(
+        id = "607d965abd1c9fadaa88f2ff",
+        number = 1,
+        graphic = "",
+        minXP = 0,
+        maxXP = 99
+    )
+}
